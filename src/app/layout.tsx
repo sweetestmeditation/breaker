6186ab9b6e78881a086cad6014c4b13fb8476aa8
sweetestmeditation <brendan.ngwanforbi@gmail.com>
2024-03-21 01:39:03 -0700

    feat: Implemented RootLayout component with dynamic rendering of Navbar and authModal components. Added global styles and set metadata for the website.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
+  authModal
 }: {
   children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
     <html lang='en' 
@@ -23,7 +25,10 @@ export default function RootLayout({
         inter.className
       )}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        {/* @ts-expect-error server component */}
         <Navbar /> {/* Render Navbar component here */}
+
+        {authModal}
 
         <div className='container max-w-7xl mx-auto h-full pt-12'>
           {children}
