f928f40ab2720febbb875f500af21ae34ebe31ba
sweetestmeditation <brendan.ngwanforbi@gmail.com>
2024-03-23 20:21:03 -0700

    feat: added Your Feed title and structured feed layout with subreddit information in Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-    return <div>Home</div>
-  }
+    return (
+      <>
+        <h1 className='font-bold text-3xl md:text-4xl'>Your Feed</h1>
+        <div className="grid grid cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+          {/* feed */}
+
+          {/* subreddit info */}
+          <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+            
+          </div>
+        </div>
+      </>
+    )
+}
